0ecb6788b30f80d61609ef2cba407afda77d1fcd
Brendan <2bndy5@gmail.com>
2022-05-18 15:05:05 -0700

    remove unused import (again)

--- sphinx_immaterial/search_adapt.py
+++ sphinx_immaterial/search_adapt.py
@@ -10,7 +10,7 @@
 """
 
 import io
-from typing import cast, Any, Dict, IO, List, Tuple, Union, Optional, Sequence
+from typing import cast, Any, Dict, IO, List, Tuple, Union, Optional
 
 import sphinx
 import sphinx.search
