jbms/sphinx-immaterial

commit 8e4485e69364830a4020618899a71623324b284f

Add python_apigen extension
Co-authored-by: Brendan <2bndy5@gmail.com>

tests/python_apigen_test.py
@@ -1,0 +1,82 @@
+import pathlib
+
+import pytest
+
+from sphinx_immaterial.python_apigen import _get_api_data
+
+from sphinx.testing.path import path as SphinxPath
+
+pytest_plugins = ("sphinx.testing.fixtures",)
+
+
+@pytest.fixture
+def apigen_make_app(tmp_path: pathlib.Path, make_app):
+
+    conf = """
+extensions = [
+    "sphinx_immaterial",
+    "sphinx_immaterial.python_apigen",
+]
+html_theme = "sphinx_immaterial"
+"""
+
+    def make(extra_conf: str = "", **kwargs):
+        (tmp_path / "conf.py").write_text(conf + extra_conf, encoding="utf-8")
+        (tmp_path / "index.rst").write_text("", encoding="utf-8")
+        return make_app(srcdir=SphinxPath(str(tmp_path)), **kwargs)
+
+    yield make
+
+
+@pytest.mark.parametrize(
+    "order_tiebreaker,expected_members",
+    [("alphabetical", ["a", "b"]), ("definition_order", ["b", "a"])],
+)
+def test_alphabetical(apigen_make_app, order_tiebreaker, expected_members):
+
+    app = apigen_make_app(
+        confoverrides=dict(
+            python_apigen_order_tiebreaker=order_tiebreaker,
+            python_apigen_default_groups=[],
+            python_apigen_modules={
+                "python_apigen_test_modules.alphabetical": "api/",
+            },
+        ),
+    )
+
+    print(app._status.getvalue())
+    print(app._warning.getvalue())
+
+    data = _get_api_data(app.env)
+    members = [x.name for x in data.top_level_groups["public-members"]]
+    assert members == expected_members
+
+
+def test_classmethod(apigen_make_app):
+    testmod = "python_apigen_test_modules.classmethod"
+    app = apigen_make_app(
+        confoverrides=dict(
+            python_apigen_default_groups=[
+                ("method:.*", "Methods"),
+                ("classmethod:.*", "Class methods"),
+                ("staticmethod:.*", "Static methods"),
+            ],
+            python_apigen_modules={
+                testmod: "api/",
+            },
+        ),
+    )
+
+    print(app._status.getvalue())
+    print(app._warning.getvalue())
+
+    data = _get_api_data(app.env)
+
+    # FIXME: Currently all methods are assigned to the 'Methods" group because
+    # the `classmethod` and `staticmethod` object types aren't actually used by
+    # Sphinx.
+    #
+    # https://github.com/sphinx-doc/sphinx/issues/3743
+    assert data.entities[f"{testmod}.Foo.my_method"].group_name == "Methods"
+    assert data.entities[f"{testmod}.Foo.my_staticmethod"].group_name == "Methods"
+    assert data.entities[f"{testmod}.Foo.my_classmethod"].group_name == "Methods"

sphinx_immaterial/autodoc_property_type.py
@@ -1,12 +1,14 @@
 """Adds support for obtaining property types from docstring signatures, and
 improves formatting by PyProperty of type annotations."""
 import re
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Any
 
 import sphinx.addnodes
 import sphinx.domains
 import sphinx.domains.python
 import sphinx.ext.autodoc
+import sphinx.util.inspect
+import sphinx.util.typing
 
 PropertyDocumenter = sphinx.ext.autodoc.PropertyDocumenter
 
@@ -14,10 +16,17 @@ PropertyDocumenter = sphinx.ext.autodoc.PropertyDocumenter
 property_sig_re = re.compile("^(\\(.*)\\)\\s*->\\s*(.*)$")
 
 
-def _get_property_return_type(obj: property) -> Optional[str]:
-    fget = getattr(obj, "fget", None)
-    if fget is not None:
-        obj = fget
+def _get_property_getter(obj: Any) -> Any:
+    # property
+    func = sphinx.util.inspect.safe_getattr(obj, "fget", None)
+    if func is None:
+        # cached_property
+        func = sphinx.util.inspect.safe_getattr(obj, "func", None)
+    return func
+
+
+def _get_property_return_type(obj: Any) -> Optional[str]:
+    fget = _get_property_getter(obj)
     doc = obj.__doc__
     if doc is None:
         return None
@@ -40,6 +49,25 @@ def _apply_property_documenter_type_annotation_fix():
         result = orig_import_object(self, raiseerror)
         if not result:
             return False
+
+        func = _get_property_getter(self.object)
+
+        if func is not None:
+            try:
+                signature = sphinx.util.inspect.signature(
+                    func, type_aliases=self.config.autodoc_type_aliases
+                )
+                if (
+                    signature.return_annotation
+                    is not sphinx.util.inspect.Parameter.empty
+                ):
+                    self.retann = sphinx.util.typing.stringify(
+                        signature.return_annotation
+                    )
+                return True
+            except TypeError:
+                pass
+
         if not self.retann:
             self.retann = _get_property_return_type(self.object)  # type: ignore
         return True

.github/workflows/build.yml
@@ -55,6 +55,8 @@ jobs:
           dist/*.tar.*
     - name: Install wheel and docs' dependencies
       run: pip install dist/*.whl -r docs/requirements.txt
+    - run: pytest -vv -s tests/
+      name: Run Python tests
     - name: Build html docs
       working-directory: docs
       run: sphinx-build . _build/html

.gitignore
@@ -14,7 +14,7 @@ __pycache__/
 
 # Sphinx documentation
 docs/_build/
-docs/generated/
+docs/python_apigen_generated/
 
 # mypy
 .mypy_cache/

.pylintrc
@@ -111,7 +111,8 @@ disable=raw-checker-failed,
         too-many-instance-attributes,
         bad-option-value,
         too-many-return-statements,
-        line-too-long
+        line-too-long,
+        fixme
 
 # Enable the message, report, category or checker with the given id(s). You can
 # either give multiple identifier separated by comma (,) or put this option

docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
@@ -54,6 +55,7 @@ extensions = [
     "sphinx_immaterial.format_signatures",
     "sphinx_immaterial.cppreference",
     "sphinx_immaterial.json_domain",
+    "sphinx_immaterial.python_apigen",
     "sphinx_jinja",
 ]
 
@@ -65,9 +67,6 @@ intersphinx_mapping = {
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
 default_role = "any"
-
-autosummary_generate = True
-autoclass_content = "class"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -228,6 +227,10 @@ rst_prolog = """
 .. role:: json(code)
    :language: json
    :class: highlight
+
+.. role:: rst(code)
+   :language: rst
+   :class: highlight
 """
 
 
@@ -276,6 +279,15 @@ jinja_contexts = {
 
 json_schemas = ["index_transform_schema.yml", "inheritance_schema.yml"]
 
+python_apigen_modules = {"tensorstore_demo": "python_apigen_generated/"}
+
+python_apigen_default_groups = [
+    ("class:.*", "Classes"),
+    (r".*:.*\.__(init|new)__", "Constructors"),
+    (r".*:.*\.__eq__", "Comparison operators"),
+    (r".*:.*\.__(str|repr)__", "String representation"),
+]
+
 
 def _validate_parallel_build(app):
     # Verifies that all of the extensions defined by this theme support parallel
@@ -291,7 +303,7 @@ def _parse_object_description_signature(
     registry_option = registry.get(signature)
     node += sphinx.addnodes.desc_name(signature, signature)
     if registry_option is None:
-        logger.error("Invalid object description option: %r", signature)
+        logger.error("Invalid object description option: %r", signature, location=node)
     else:
         node += sphinx.addnodes.desc_sig_punctuation(" : ", " : ")
         annotations = sphinx.domains.python._parse_annotation(
@@ -316,7 +328,7 @@ def _parse_confval_signature(
     registry_option = values.get(signature)
     node += sphinx.addnodes.desc_name(signature, signature)
     if registry_option is None:
-        logger.error("Invalid config option: %r", signature)
+        logger.error("Invalid config option: %r", signature, location=node)
     else:
         default, rebuild, types = registry_option
         if isinstance(types, sphinx.config.ENUM):
@@ -364,5 +376,14 @@ def setup(app):
         objname="object description option",
         indextemplate="pair: %s; object description option",
         parse_node=_parse_object_description_signature,
+    )
+
+    # Add `event` type from Sphinx's own documentation, to allow intersphinx
+    # references to Sphinx events.
+    app.add_object_type(
+        "event",
+        "event",
+        objname="Sphinx event",
+        indextemplate="pair: %s; event",
     )
     app.connect("builder-inited", _validate_parallel_build)

docs/tensorstore_demo/_tensorstore.py
@@ -1,0 +1,58 @@
+"""Demo file for Python API documentation generation.
+
+This defines some entities that are aliased into the `tensorstore_demo` module.
+"""
+
+import typing
+
+
+class Foo:
+    """
+    This is a class defined in the ``tensorstore_demo._tensorstore`` module
+    but should appear to be defined in the ``tensorstore_demo`` module.
+
+    Group:
+      Some other group
+    """
+
+    def bar(self, x: int) -> int:
+        """Returns :py:param:`x`.
+
+        :param x: Parameter to :py:obj:`.bar`.
+        """
+        pass
+
+
+def bar(x: Foo) -> Foo:
+    """Returns :py:param:`x`.
+
+    :param x: Parameter to :py:obj:`.bar`.
+
+    Group:
+      Some other group.
+    """
+    pass
+
+
+class FooSubclass(Foo):
+    """This is a subclass of :py:obj:`.Foo`.
+
+    Group:
+      Some other group.
+    """
+
+    def baz(self, x: str) -> str:
+        """Does the baz operation."""
+        return x
+
+    @typing.overload
+    def overloaded_method(self, x: int) -> int:
+        pass
+
+    @typing.overload
+    def overloaded_method(self, x: str) -> str:
+        pass
+
+    def overloaded_method(self, x):
+        """Returns its argument."""
+        return x

commit 1d6919fd84b366757f836b9cae12256efceec21b

Fix regression in extracting return type from pybind11 properties
This regression was introduced by
8e4485e69364830a4020618899a71623324b284f.

.clang-format
@@ -1,0 +1,1 @@
+BasedOnStyle: "Google"

sphinx_immaterial/apidoc/python/autodoc_property_type.py
@@ -25,9 +25,11 @@ def _get_property_getter(obj: Any) -> Any:
     return func
 
 
-def _get_property_return_type(obj: Any) -> Optional[str]:
+def _get_return_type_from_fget_doc(obj: Any) -> Optional[str]:
     fget = _get_property_getter(obj)
-    doc = obj.__doc__
+    if fget is None:
+        return None
+    doc = sphinx.util.inspect.safe_getattr(fget, "__doc__", None)
     if doc is None:
         return None
     line = doc.splitlines()[0]
@@ -69,7 +71,9 @@ def apply_property_documenter_type_annotation_fix():
                 pass
 
         if not self.retann:
-            self.retann = _get_property_return_type(self.object)  # type: ignore
+            new_retann = _get_return_type_from_fget_doc(self.object)
+            if new_retann is not None:
+                self.retann = new_retann
         return True
 
     PropertyDocumenter.import_object = import_object
@@ -81,7 +85,7 @@ def apply_property_documenter_type_annotation_fix():
         old_add_directive_header(self, sig)
 
         # Check for return annotation
-        retann = self.retann or _get_property_return_type(self.object)
+        retann = self.retann or _get_return_type_from_fget_doc(self.object)
         if retann is None:
             return
 

tests/issue_134/sphinx-immaterial-pybind11-issue-134/sphinx_immaterial_pybind11_issue_134.cpp
@@ -1,31 +1,36 @@
 #include <pybind11/pybind11.h>
 
-struct Example
-{
-    const bool is_set_by_init;
-    Example() : is_set_by_init(false) {}
-    Example(const bool value) : is_set_by_init(value) {}
+struct Example {
+  const bool is_set_by_init;
+  Example() : is_set_by_init(false) {}
+  Example(const bool value) : is_set_by_init(value) {}
 };
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(sphinx_immaterial_pybind11_issue_134, m)
-{
+PYBIND11_MODULE(sphinx_immaterial_pybind11_issue_134, m) {
+  py::class_<Example> cls(m, "Example");
+
+  {
     py::options options;
     options.disable_function_signatures();
 
-    py::class_<Example>(m, "Example")
-        .def(py::init<>(), R"docstr(
-            __init__()
-            __init__(value: bool)
+    cls.def(py::init<>(), R"docstr(
+                __init__()
+                __init__(value: bool)
 
-            The default constructor takes no args.
-        )docstr")
+                The default constructor takes no args.
+            )docstr")
         .def(py::init<const bool>(), R"docstr(
-            The overloaded constructor takes 1 ``bool`` arg.
-        )docstr",
-             py::arg("value"))
-        .def_readonly("is_set_by_init", &Example::is_set_by_init, R"docstr(
+                The overloaded constructor takes 1 ``bool`` arg.
+            )docstr",
+             py::arg("value"));
+
+    cls.def_property_readonly("no_signature",
+                              [](const Example& self) -> int { return 42; });
+  }
+
+  cls.def_readonly("is_set_by_init", &Example::is_set_by_init, R"docstr(
             This read-only ``bool`` attribute is set by the constructor.
         )docstr");
 }

tests/python_apigen_test.py
@@ -98,3 +98,43 @@ def test_issue_147(apigen_make_app):
 
     for entity in data.entities.values():
         assert entity.options["module"] == testmod
+
+
+def test_pybind11_property(apigen_make_app):
+    testmod = "sphinx_immaterial_pybind11_issue_134"
+    app = apigen_make_app(
+        confoverrides=dict(
+            python_apigen_modules={
+                testmod: "api/",
+            },
+        ),
+    )
+    print(app._status.getvalue())
+    print(app._warning.getvalue())
+
+    data = _get_api_data(app.env)
+
+    options = data.entities[f"{testmod}.Example.is_set_by_init"].options
+    assert options["type"] == "bool"
+
+    options = data.entities[f"{testmod}.Example.no_signature"].options
+    assert "type" not in options
+
+
+def test_pure_python_property(apigen_make_app):
+    testmod = "python_apigen_test_modules.property"
+    app = apigen_make_app(
+        confoverrides=dict(
+            python_apigen_modules={
+                testmod: "api/",
+            },
+        ),
+    )
+
+    print(app._status.getvalue())
+    print(app._warning.getvalue())
+
+    data = _get_api_data(app.env)
+
+    options = data.entities[f"{testmod}.Example.foo"].options
+    assert options["type"] == "int"

tests/python_apigen_test_modules/property.py
@@ -1,0 +1,4 @@
+class Example:
+    @property
+    def foo(self) -> int:
+        return 42