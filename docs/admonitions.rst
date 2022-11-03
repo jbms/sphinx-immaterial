09b41f950db5b3ba62143aca14276df6c6f3fcc8
Brendan <2bndy5@gmail.com>
2022-11-02 19:36:09 -0700

    add inline icon role and doc for it (#182)
    * add inline icon role and doc for it
    * augment info about custom admonition icons
    * review requested changes
    * abstract fetching of SVG data into public function
    * second round review requests

--- docs/admonitions.rst
+++ docs/admonitions.rst
@@ -214,6 +214,8 @@ Changing the Admonition Icon
 
 Any of the above builtin admonitions' icons can be changed using the
 :themeconf:`icon`\ [:themeconf:`admonition`] field in :confval:`html_theme_options` settings.
+This will only work with `any of the icons bundled with this theme 
+<https://github.com/squidfunk/mkdocs-material/tree/master/material/.icons>`_.
 
 .. code-block:: python
    :caption: Changing the `note` icon in conf.py
@@ -229,10 +231,27 @@ Any of the above builtin admonitions' icons can be changed using the
 .. admonition:: ``seealso`` uses the ``note`` icon
    :class: missing
 
-   The `seealso` admonition (which is specific to Sphinx - not reStructuredText) will use the same
-   icon set for the `note` admonition. If you want to override the icon for the `seealso`
-   admonition, then use the tactic shown in the `Custom admonitions`_ section (with regard to
-   only the icon changes in CSS).
+   The `seealso` admonition (which is specific to Sphinx - not reStructuredText or mkdocs-material
+   theme) will use the same icon set for the `note` admonition. If you want to override only the
+   icon for the `seealso` admonition, then use the tactic shown in the `Custom admonitions`_
+   section (with regard to only the icon changes in CSS). However, instead of copying the SVG data
+   into a CSS variable, one could simply use :themeconf:`icon`\ [:themeconf:`admonition`] field.
+
+   .. code-block:: python
+
+      html_theme_options = {
+          "icon": {
+              "admonition": {
+                  "seealso": "octicons/eye-24", # (1)!
+              },
+          },
+      }
+
+   .. code-annotations::
+      #. Uses the icon :si-icon:`octicons/eye-24`
+
+   Then, use the the generated CSS variable :css:`--md-admonition-icon--seealso` in the custom CSS for
+   the :css:`mask-image` rules.
 
 .. details:: Alternate icon sets
    :class: example
---
+++ sphinx_immaterial/inline_icons.py
@@ -0,0 +1,66 @@
+"""This module adds the md-icon role. This is specific to this this theme because,
+primarily, the icons bundled with this theme are supported (plus any svg files in the
+user project's static directories)."""
+from pathlib import Path
+from typing import Tuple, List
+from docutils import nodes
+from sphinx.application import Sphinx
+from sphinx.builders import Builder
+from sphinx.writers.html5 import HTML5Translator
+
+
+def read_svg_into_span(builder: Builder, icon_name: str, classes: List[str]) -> str:
+    """Reads the SVG data into a HTML span element."""
+    svg = Path(__file__).parent / ".icons" / icon_name
+    if not svg.exists():
+        static_paths: List[str] = getattr(builder.config, "html_static_path")
+        for path in static_paths:
+            svg = Path(builder.srcdir) / path / icon_name
+            if svg.exists():
+                break
+        else:
+            raise FileNotFoundError(
+                f"{icon_name} not found in html_static_path and not bundled with theme"
+            )
+    svg_data = svg.read_text(encoding="utf-8")
+    css_classes = " ".join(classes)
+    return f'<span class="{css_classes}">{svg_data}</span>'
+
+
+class si_icon(nodes.container):
+    pass
+
+
+def visit_si_icon(self: HTML5Translator, node: si_icon):
+    """read icon data and put it into a span element"""
+    # in case this node was created from another extension
+    assert node.rawsource, "icon node's rawsource must be a relative path"
+
+    self.body.append(
+        read_svg_into_span(
+            builder=self.builder,
+            icon_name=node.rawsource + ".svg",
+            classes=node["classes"],
+        )
+    )
+    raise nodes.SkipNode()
+
+
+def icons_role(
+    role: str, rawtext: str, text: str, lineno: int, inliner, options={}, content=[]
+) -> Tuple[List[nodes.Node], List[nodes.system_message]]:
+    path, classes = (text, "")
+    if ";" in text:
+        path, classes = text.split(";")[:2]
+    div = si_icon(path, classes=["md-icon", "si-icon-inline"] + classes.split(","))
+    return [div], []
+
+
+def setup(app: Sphinx):
+
+    app.add_role("si-icon", icons_role)
+    app.add_node(si_icon, html=(visit_si_icon, None))
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
