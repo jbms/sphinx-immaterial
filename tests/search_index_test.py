43f410488372aef98829c3911f88ccdac9434807
Jeremy Maitin-Shepard <jbms@google.com>
2023-01-03 21:32:24 -0800

    Fix handling of "short" anchors in the search index
    Fixes #198.

---
+++ tests/search_index_test.py
@@ -0,0 +1,27 @@
+import json
+import os
+import pathlib
+
+import sphinx.search
+
+
+def test_object_anchor(immaterial_make_app):
+    app = immaterial_make_app(
+        files={
+            "index.rst": """
+Overall title
+=============
+
+.. py:class:: Foo
+""",
+        },
+    )
+    app.build()
+
+    assert not app._warning.getvalue()
+    search_index_js = pathlib.Path(
+        os.path.join(app.outdir, "searchindex.js")
+    ).read_text("utf-8")
+    search_index = sphinx.search.js_index.loads(search_index_js)
+
+    assert search_index["objects"] == {"": [[0, 0, 1, 0, "Foo", ""]]}
