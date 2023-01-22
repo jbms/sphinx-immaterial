9c6a2c8aa873a61b3792de118a126daef8acdf80
Jeremy Maitin-Shepard <jbms@google.com>
2023-01-21 22:53:11 -0800

    Add monkey patch to fix source file/line number info for object descriptions
    This addresses the issue described at
    https://github.com/sphinx-doc/sphinx/pull/11147.

---
+++ sphinx_immaterial/apidoc/fix_sphinx_issue_11147.py
@@ -0,0 +1,29 @@
+"""Monkey patches a fix for https://github.com/sphinx-doc/sphinx/pull/11147."""
+
+import inspect
+
+import sphinx.util.nodes
+import sphinx.util.docutils
+
+
+def _monkey_patch_nested_parse_with_titles():
+    orig_nested_parse_with_titles = sphinx.util.nodes.nested_parse_with_titles
+
+    if list(inspect.signature(orig_nested_parse_with_titles).parameters.keys()) != [
+        "state",
+        "content",
+        "node",
+    ]:
+        # Assume https://github.com/sphinx-doc/sphinx/pull/11147 has been merged.
+        return
+
+    # Note that this is different from the fix in
+    # https://github.com/sphinx-doc/sphinx/pull/11147, since it is not practical
+    # to monkey patch all callers to pass in the `content_offset`.  Instead, we
+    # use `switch_source_input`, which makes it unnecessary to pass in
+    # `content_offset`.
+    def nested_parse_with_titles(state, content, node):
+        with sphinx.util.docutils.switch_source_input(state, content):
+            return orig_nested_parse_with_titles(state, content, node)
+
+    sphinx.util.nodes.nested_parse_with_titles = nested_parse_with_titles
