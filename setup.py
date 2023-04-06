c531938adce50ec2dc87f513cad8659318244456
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2023-04-06 16:24:43 +0300

    Lower the bound for python version

--- setup.py
+++ setup.py
@@ -197,7 +197,7 @@ setuptools.setup(
         ],
         "sphinx_immaterial.apidoc.cpp.cppreference_data": ["*.xml"],
     },
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=REQUIREMENTS,
     use_scm_version={
         # It would be nice to include the commit hash in the version, but that
