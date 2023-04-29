90953ad51b9da870f29721c63fd600f1efd684ce
Brendan <2bndy5@gmail.com>
2023-04-28 22:48:27 -0700

    copy mathjax dist in env-updated event

--- sphinx_immaterial/local_mathjax.py
+++ sphinx_immaterial/local_mathjax.py
@@ -5,15 +5,10 @@ from sphinx.application import Sphinx
 from sphinx.config import Config
 from sphinx.errors import ExtensionError
 from sphinx.ext.mathjax import MATHJAX_URL, MathDomain
+from sphinx.environment import BuildEnvironment
 
 
-def copy_mathjax_dist(
-    app: Sphinx,
-    pagename: str,
-    templatename: str,
-    context: Dict[str, Any],
-    event_arg: Any,
-) -> None:
+def copy_mathjax_dist(app: Sphinx, env: BuildEnvironment) -> None:
     if (
         app.builder.format != "html"
         or app.builder.math_renderer_name != "mathjax"  # type: ignore[attr-defined]
@@ -29,15 +24,13 @@ def copy_mathjax_dist(
     ):
         return  # not using theme's cache of mathjax dist
 
-    domain = cast(MathDomain, app.env.get_domain("math"))
-    if app.registry.html_assets_policy == "always" or domain.has_equations(pagename):
-        # copy mathjax fonts only if equations exists (and not already done)
-        dst = Path(app.outdir, "_static", "mathjax")
-        if Path(app.outdir, "_static", dst).exists():
-            return
+    domain = cast(MathDomain, env.get_domain("math"))
+    if app.registry.html_assets_policy == "always" or domain.has_equations():
+        # copy mathjax fonts only if equations exists
         shutil.copytree(
             str(Path(__file__).parent / "static" / "mathjax"),
-            str(dst),
+            str(Path(app.outdir, "_static", "mathjax")),
+            dirs_exist_ok=True,  # for consecutive builds
         )
 
 
@@ -57,7 +50,7 @@ def _config_inited(app: Sphinx, config: Config) -> None:
 
 def setup(app: Sphinx):
     app.connect("config-inited", _config_inited)
-    app.connect("html-page-context", copy_mathjax_dist)
+    app.connect("env-updated", copy_mathjax_dist)
     return {
         "parallel_read_safe": True,
         "parallel_write_safe": True,
