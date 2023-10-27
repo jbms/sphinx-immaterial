0d83cf390481b8443360ded6fd8a02ce538b5d9f
Ilya Boyazitov <kvabrat@gmail.com>
2023-10-28 01:42:56 +0300

    Add support for "scope" option (#284)
    * Add support for "scope" option
    
    * Add test for "extra.scope" option
    
    * Clarify description for "scope"
    
    * Add fixes for nox
    
    * Add parametrization for test_extra_scope, fix bug for "scope": None
    
    * Update docs/customization.rst
    
    Co-authored-by: Brendan <2bndy5@gmail.com>
    
    * Add fixes for nox checks
    
    * Add app._warning in extra_scope_test
    
    * Add SSL packages
    
    * Move SSL packages to main requirements.txt
    
    * Remove unnecessary packages
    
    ---------
    
    Co-authored-by: Bizordec <kvabart@gmail.com>
    Co-authored-by: Brendan <2bndy5@gmail.com>

---
+++ tests/extra_scope_test.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import re
+import textwrap
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pytest
+from bs4 import BeautifulSoup
+
+if TYPE_CHECKING:
+    from sphinx.testing.util import SphinxTestApp
+
+
+@pytest.mark.parametrize(
+    "scope_url,expected_scope_url",
+    [
+        (None, "."),
+        ("", ""),
+        ("/", "/"),
+        ("/subsite", "/subsite"),
+    ],
+    ids=[
+        "base_url",
+        "empty",
+        "root",
+        "subsite",
+    ],
+)
+def test_extra_scope(
+    immaterial_make_app,
+    scope_url: str | None,
+    expected_scope_url: str,
+):
+    if scope_url is not None:
+        scope_url = f'"{scope_url}"'
+
+    app: SphinxTestApp = immaterial_make_app(
+        extra_conf=textwrap.dedent(
+            f"""
+            html_theme_options = {{
+                "scope": {scope_url}
+            }}
+            """
+        ),
+        files={
+            "index.rst": "Sample text",
+        },
+    )
+    app.build()
+
+    assert not app._warning.getvalue()
+
+    with open(Path(app.outdir) / "index.html", mode="r") as file:
+        soup = BeautifulSoup(file.read(), "html.parser")
+
+    head = soup.head
+    assert head is not None
+
+    scope_pattern = re.compile(r"__md_scope=new URL\(\"(?P<url>[^\"]+)\"")
+    matched_scope_url = ""
+    for script in head.find_all("script"):
+        scope_match = scope_pattern.search(script.text)
+        if scope_match:
+            matched_scope_url = scope_match.group("url")
+            break
+
+    assert matched_scope_url == expected_scope_url
