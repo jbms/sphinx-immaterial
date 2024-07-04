152764121cc383f302b6ede67ef64531582ce87e
Brendan <2bndy5@gmail.com>
2024-07-04 02:00:53 -0700

    test code annotations

---
+++ tests/code_annotations_test.py
@@ -0,0 +1,50 @@
+"""Tests related to theme's code block annotations."""
+
+from sphinx.testing.util import SphinxTestApp
+
+index_rst = """
+The Test
+========
+
+.. code-block:: yaml
+    :caption: A caption for good measure
+
+    # (1)!
+
+.. code-annotations::
+    1. An annotation
+
+.. code-annotations::
+    #. A regular list not used in annotations.
+
+.. code-block:: python
+
+    # A normal code block without annotations
+"""
+
+
+def test_code_annotation(immaterial_make_app):
+    app: SphinxTestApp = immaterial_make_app(
+        files={"index.rst": index_rst},
+    )
+
+    app.build()
+    assert not app._warning.getvalue()  # type: ignore[attr-defined]
+
+def test_code_annotation_error(immaterial_make_app):
+    app: SphinxTestApp = immaterial_make_app(
+        files={
+            "index.rst": """
+The Test
+========
+
+.. code-annotations::
+    This is not an enumerated list!
+
+"""},
+    )
+
+    app.build()
+    warnings = app._warning.getvalue()  # type: ignore[attr-defined]
+    assert warnings
+    assert "The code-annotations directive only accepts an enumerated list" in warnings
