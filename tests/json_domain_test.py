6c74d6affd780635eced002833e1b6e2494effd8
Brendan <2bndy5@gmail.com>
2024-07-04 12:14:41 -0700

    improve json domain coverage

--- tests/json_domain_test.py
+++ tests/json_domain_test.py
@@ -1,7 +1,57 @@
+from pathlib import Path
 import re
 
 import docutils.utils
+import pytest
 import sphinx.addnodes
+
+
+@pytest.mark.parametrize(
+    "doc,schema",
+    [
+        (
+            "\n".join(
+                [
+                    ".. json:schema:: Pet",
+                    ".. json:schema:: Dog",
+                    ".. json:schema:: Cat",
+                ]
+            ),
+            "docs/apidoc/json/inheritance_schema.yml",
+        ),
+        (
+            "\n".join(
+                [
+                    ".. json:schema:: IndexTransform",
+                    ".. json:schema:: OutputIndexMap",
+                    ".. json:schema:: IndexInterval",
+                ]
+            ),
+            "docs/apidoc/json/index_transform_schema.yml",
+        ),
+    ],
+    ids=["inheritance", "IndexTransform"],
+)
+def test_doc_examples(doc: str, schema: str, tmp_path: Path, immaterial_make_app):
+    schema_path = Path(__file__).parent.parent / schema
+    app = immaterial_make_app(
+        extra_conf='''
+extensions.append("sphinx_immaterial.apidoc.json.domain")
+json_schemas = ["schema.yml"]
+json_schema_validate = True
+rst_prolog = """
+.. role:: json(code)
+    :language: json
+    :class: highlight
+"""
+''',
+        files={
+            "index.rst": doc,
+            "schema.yml": schema_path.read_text(encoding="utf-8"),
+        },
+    )
+    app.build()
+    assert not app._warning.getvalue()
 
 
 def test_xref_source_info(immaterial_make_app):
