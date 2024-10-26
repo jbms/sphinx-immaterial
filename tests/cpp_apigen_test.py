fcc5f762226cd544eed166ab2c24c7d8f54c8eb7
Jeremy Maitin-Shepard <jbms@google.com>
2024-10-25 22:21:51 -0700

    Fix C++ apigen support for macros with Sphinx >= 7.4 (#395)

--- tests/cpp_apigen_test.py
+++ tests/cpp_apigen_test.py
@@ -36,3 +36,39 @@ constexpr inline bool IsArray = false;
     app.build()
 
     assert not app._warning.getvalue()
+
+
+def test_macro(immaterial_make_app):
+    app = immaterial_make_app(
+        extra_conf="""
+extensions.append("sphinx_immaterial.apidoc.cpp.apigen")
+""",
+        confoverrides=dict(
+            nitpicky=True,
+            cpp_apigen_configs=[
+                dict(
+                    document_prefix="cpp_apigen_generated/",
+                    api_parser_config=dict(
+                        input_content=r"""
+/// Tests something.
+///
+/// \ingroup Array
+#define IS_ARRAY(x) x + 1
+""",
+                        compiler_flags=["-std=c++17", "-x", "c++"],
+                        verbose=True,
+                    ),
+                ),
+            ],
+        ),
+        files={
+            "index.rst": """
+.. cpp-apigen-group:: Array
+
+"""
+        },
+    )
+
+    app.build()
+
+    assert not app._warning.getvalue()
