045dbd668f93a023c32dbbf489345a3645e42053
Brendan <2bndy5@gmail.com>
2025-05-09 19:47:15 -0700

    resolves #441
    added a test for regression checking

--- sphinx_immaterial/local_mathjax.py
+++ sphinx_immaterial/local_mathjax.py
@@ -2,6 +2,8 @@ import shutil
 from pathlib import Path
 from typing import Any, Dict, cast
 
+import docutils.nodes
+from sphinx import version_info as sphinx_version
 from sphinx.application import Sphinx
 from sphinx.config import Config
 from sphinx.domains.math import MathDomain
@@ -9,8 +11,26 @@ from sphinx.environment import BuildEnvironment
 from sphinx.errors import ExtensionError
 from sphinx.ext.mathjax import MATHJAX_URL
 
+_USES_MATHJAX_KEY = "SPHINX_IMMATERIAL_USES_MATHJAX"
 
-def copy_mathjax_dist(app: Sphinx, env: BuildEnvironment) -> None:
+
+def page_has_equations(
+    app: Sphinx,
+    pagename: str,
+    templatename: str,
+    context: dict,
+    doctree: docutils.nodes.Node,
+) -> None:
+    """Determines if mathjax dist is need on a per page basis.
+
+    Only called when sphinx is v8.2 or newer.
+    """
+
+    if context.get("has_maths_elements", False):
+        setattr(app.builder.env, _USES_MATHJAX_KEY, True)
+
+
+def copy_mathjax_dist(app: Sphinx) -> None:
     if (
         app.builder.format != "html" or app.builder.math_renderer_name != "mathjax"  # type: ignore[attr-defined]
     ):
@@ -25,14 +45,29 @@ def copy_mathjax_dist(app: Sphinx, env: BuildEnvironment) -> None:
     ):
         return  # not using theme's cache of mathjax dist
 
-    domain = cast(MathDomain, env.get_domain("math"))
-    if app.registry.html_assets_policy == "always" or domain.has_equations():
+    uses_mathjax = False
+    if sphinx_version[:3] < (8, 2):
+        domain = cast(MathDomain, app.env.get_domain("math"))
+        uses_mathjax = domain.has_equations()
+    elif getattr(app.env, _USES_MATHJAX_KEY, False) is True:
+        uses_mathjax = True
+    if app.registry.html_assets_policy == "always" or uses_mathjax:
         # copy mathjax fonts only if equations exists
         shutil.copytree(
             str(Path(__file__).parent / "bundles" / "mathjax"),
             str(Path(app.outdir, "_static", "mathjax")),
             dirs_exist_ok=True,  # for consecutive builds
         )
+
+
+def build_finished(app: Sphinx, exc: Exception | None):
+    """Only called when sphinx is v8.2 or newer."""
+    copy_mathjax_dist(app)
+
+
+def env_updated(app: Sphinx, env: BuildEnvironment):
+    """Only called when sphinx is older than v8.2."""
+    copy_mathjax_dist(app)
 
 
 def _config_inited(app: Sphinx, config: Config) -> None:
@@ -51,7 +86,11 @@ def _config_inited(app: Sphinx, config: Config) -> None:
 
 def setup(app: Sphinx):
     app.connect("config-inited", _config_inited)
-    app.connect("env-updated", copy_mathjax_dist)
+    if sphinx_version[:3] >= (8, 2):
+        app.connect("html-page-context", page_has_equations)
+        app.connect("build-finished", build_finished)
+    else:
+        app.connect("env-updated", env_updated)
     return {
         "parallel_read_safe": True,
         "parallel_write_safe": True,
