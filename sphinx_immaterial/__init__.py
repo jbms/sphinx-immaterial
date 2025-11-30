2a6f2f9071bd5b014280a12163b723dc8a2988bf
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2025-11-30 04:50:59 +0000

    Update sphinx_immaterial/__init__.py

--- sphinx_immaterial/__init__.py
+++ sphinx_immaterial/__init__.py
@@ -68,9 +68,16 @@ def _get_html_builder(base_builder: Type[sphinx.builders.html.StandaloneHTMLBuil
         theme: sphinx.theming.Theme
         templates: sphinx.jinja2glue.BuiltinTemplateLoader
 
-        default_translator_class = html_translator_mixin.get_html_translator(
-            base_builder.default_translator_class
-        )
+        if version_info >= (7, 0):
+            default_translator_class = html_translator_mixin.get_html_translator(
+                base_builder.default_translator_class
+            )
+        else:
+            @property
+            def default_translator_class(self):
+                return html_translator_mixin.get_html_translator(
+                    super().default_translator_class
+                )
 
         def init_js_files(self):
             super().init_js_files()
