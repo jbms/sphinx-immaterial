812e9c05e386da434d049cada63c33656f296acc
Brendan <2bndy5@gmail.com>
2025-12-06 05:17:51 -0800

    revert docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -512,8 +512,8 @@ nitpick_ignore = [
     ("cpp:identifier", "my_ns3"),
     ("cpp:identifier", "cpp_apigen_demo"),
     # Example JavaScript types
-    ("js:class", "string"),
-    ("js:class", "SomeError"),
+    ("js:func", "string"),
+    ("js:func", "SomeError"),
     # pydantic_extra_types.color not present in object inventory
     ("py:class", "pydantic_extra_types.color.Color"),
 ]
